b66e0dc64d4ce89c13b02928d4a5a73a3bff2435
Suraj Jha <121048122+surajjha028@users.noreply.github.com>
2023-06-18 12:43:52 +0530

    Example of undefined-notdefined

---
+++ undefined-notdefined.js
@@ -0,0 +1,11 @@
+var a;
+console.log(a); // undefined
+
+console.log(b); // not-defined
+
+// here we have assign a number and then have put string to it.
+a = 10;
+console.log(a);
+a = "hello world"
+console.log(a);
+
